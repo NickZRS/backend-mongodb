a34ac99dfde9b8e205f7dc3314f2326dcc9bd473
NickZRS <nick1804js@gmail.com>
2023-09-04 12:31:30 -0300

    Terceiro_Commit

--- api/index.js
+++ api/index.js
@@ -6,6 +6,9 @@ const port = 4000
 app.use(express.json())//irá fazer o parse de arquivos JSON
 //Rotas de conteúdo público
 app.use('/', express.static('public'))
+//Configura o Favicon
+app.use('/favicon.ico', express.static('public/images/computador.png'))
+
 //Rotas de API
 app.get('/api', (req, res) =>{
     res.status(200).json({
@@ -13,6 +16,17 @@ app.get('/api', (req, res) =>{
         version: '1.0.0'
     })
 })
+//Rotas de Exceção - deve ser a última
+app.use(function(req, res){
+    res.sendStatus(404).json({
+        errors: [{
+            value: `${req.originalUrl}`,
+            msg: `A rota ${req.originalUrl} não existe nesta API!`,
+            param: 'invalid route'
+        }]
+    })
+})
+
 app.listen(port, function(){
     console.log(`Servidor rodando na porta ${port}`)
 })
